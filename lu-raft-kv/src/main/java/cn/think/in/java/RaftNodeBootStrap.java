3e6ec348ac7924373de9d7571ce5022e4156ef2f
rensailong <rensailong@kuaishou.com>
2020-12-01 20:02:37 +0800

    support redis

--- lu-raft-kv/src/main/java/cn/think/in/java/RaftNodeBootStrap.java
+++ lu-raft-kv/src/main/java/cn/think/in/java/RaftNodeBootStrap.java
@@ -31,7 +31,7 @@ public class RaftNodeBootStrap {
 
         // 其他节点地址
         config.setPeerAddrs(Arrays.asList(peerAddr));
-        config.setStateMachineSaveType(StateMachineSaveType.REDIS);
+        config.setStateMachineSaveType(StateMachineSaveType.ROCKS_DB);
         Node node = DefaultNode.getInstance();
         node.setConfig(config);
 
