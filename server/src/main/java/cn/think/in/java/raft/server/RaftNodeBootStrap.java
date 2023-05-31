f3c8e87f877008e0b13f91a96f805139bd95b114
xuanmie <xuanmie@tsign.cn>
2023-05-31 10:50:27 +0800

    opt

--- server/src/main/java/cn/think/in/java/raft/server/RaftNodeBootStrap.java
+++ server/src/main/java/cn/think/in/java/raft/server/RaftNodeBootStrap.java
@@ -24,6 +24,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 
+import static cn.think.in.java.raft.server.config.Constant.*;
+
 /**
  * -DserverPort=8775
  * -DserverPort=8776
@@ -34,24 +36,26 @@ import java.util.Arrays;
 @Slf4j
 public class RaftNodeBootStrap {
 
+    public static final String[] DEFAULT_PROCESS = new String[]{"localhost:8775", "localhost:8776", "localhost:8777", "localhost:8778", "localhost:8779"};
+
     public static void main(String[] args) throws Throwable {
         boot();
     }
     
     public static void boot() throws Throwable {
-        String property = System.getProperty("cluster.addr.list");
+        String property = System.getProperty(CLUSTER_ADDR_LIST);
         String[] peerAddr;
 
         if (StringUtil.isNullOrEmpty(property)) {
-            peerAddr = new String[]{"localhost:8775", "localhost:8776", "localhost:8777", "localhost:8778", "localhost:8779"};
+            peerAddr = DEFAULT_PROCESS;
         } else {
-            peerAddr = property.split(",");
+            peerAddr = property.split(SPLIT);
         }
 
         NodeConfig config = new NodeConfig();
 
         // 自身节点
-        config.setSelfPort(Integer.parseInt(System.getProperty("serverPort", "8779")));
+        config.setSelfPort(Integer.parseInt(System.getProperty(SERVER_PORT, "8779")));
 
         // 其他节点地址
         config.setPeerAddrs(Arrays.asList(peerAddr));
---
+++ server/src/main/java/cn/think/in/java/raft/server/config/Constant.java
@@ -0,0 +1,16 @@
+package cn.think.in.java.raft.server.config;
+
+/**
+ * @version 1.0
+ * @Author cxs
+ * @Description
+ * @date 2023/5/31
+ **/
+public class Constant {
+
+    public static final String CLUSTER_ADDR_LIST = "cluster.addr.list";
+
+    public static final String SERVER_PORT = "serverPort";
+
+    public static final String SPLIT = ",";
+}
